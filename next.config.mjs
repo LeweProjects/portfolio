794037f6eac34d3ba7cf6123902365c7b3b2e3c7
Marvell Ayon <loewiayon12@gmail.com>
2024-05-09 13:08:21 +0800

    debugging

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // "compilerOptions": {
-  //   "allowImportingTsExtensions": true
+  //   "allowImportingTsExtensions": "iKnowWhatImDoing"
   // }
 };
 
