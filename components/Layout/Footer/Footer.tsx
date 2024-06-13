76b824a29868bcd2989f8fafa497c03ef84b96fe
Marvell Ayon <loewiayon12@gmail.com>
2024-06-13 10:46:33 +0800

    footer responsive update

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -15,37 +15,33 @@ const Footer: React.FC<headerProps> = ({ contacts }) => {
 
   return (
     <div
-      ref={contacts} className="flex flex-col h-[35rem] items-center w-full bg-zinc-900">
-      <div className="flex items-center w-[85%] py-7">
-        <div className="-mr-[68px]">
+      ref={contacts} id="contacts" className="flex footerContainer flex-col h-[35rem] items-center w-full bg-zinc-900">
+      <div className="flex footLeft items-center w-[85%] py-7">
+        <div className="-mr-[68px] footLogo">
           {/* @ts-ignore */}
           <Logo />
         </div>
-        <div className="flex flex-col items-start justify-center ">
+        <div className="flex flex-col footTextLeft items-start justify-center ">
           <span className="text-lg">Need help for starting a project?</span>
-          <h1 className="text-[75px] font-semibold w-full text-[#02A9F7]">{"LET'S"} TALK</h1>
-          <div className="flex items-center ">
-            <MdEmail
-              className="icon text-2xl mr-2" />
-            <p className="text-md"> loewiayon12@gmail.com</p>
+          <h1 className="text-[75px] letsTalk font-semibold w-full text-[#02A9F7]">{"LET'S"} TALK</h1>
+          <div className="flex flex-col items-start contact">
+            <div className="flex items-center ">
+              <MdEmail
+                className="icon text-2xl mr-2" />
+              <p className="text-md"> loewiayon12@gmail.com</p>
+            </div>
+            <div
+              className="flex items-center mt-3">
+              <FaPhoneAlt
+                className="icon text-2xl mr-2" />
+              <p className="text-md"> +639455963805</p>
+            </div>
           </div>
-          <div
-            className="flex items-center mt-3">
-            <FaPhoneAlt
-              className="icon text-2xl mr-2" />
-            <p className="text-md"> +639455963805</p>
-          </div>
-        </div>
-
-        <div
-          id="contacts"
-          className="h-full footerContainer flex justify-between items-center px-28"
-        >
 
         </div>
       </div>
-      <div className="border-t-2 border-white w-[80%] flex h-full justify-between -mt-5 px-16 items-center">
-        <p className="text-md font-sans">© 2024 portfolio by Marvell Ayon |<span className="font-semibold"> All Rights Reserved</span>  | Powered by <span>Next JS</span> </p>
+      <div className="border-t-2 border-white w-[80%] footerTextBottom flex h-full justify-between -mt-5 px-16 items-center">
+        <p className="text-md font-sans cred">© 2024 portfolio by Marvell Ayon |<span className="font-semibold"> All Rights Reserved</span>  | Powered by <span>Next JS</span> </p>
         <div className="medias text-right">
           <ul className={`${inter.className} flex justify-end space-x-8 text-3xl`}>
             <Link
