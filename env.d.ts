bd8c36c9f14a4f641d0770937ffd474d9bad0a2e
Marvell Ayon <loewiayon12@gmail.com>
2024-06-24 04:11:29 +0800

    env value change

--- env.d.ts
+++ env.d.ts
@@ -2,7 +2,8 @@ namespace NodeJS {
   interface ProcessEnv {
     //MONGODB_URI: "mongodb+srv://marvelladmin:footlonghotdog12@cluster0.yrsd5oh.mongodb.net/portfolio?retryWrites=true&w=majority&appName=Cluster0";
     MONGODB_URI: "mongodb+srv://marvelladmin:footlonghotdog12@cluster0.yrsd5oh.mongodb.net";
-    NEXT_PUBLIC_DOMAIN: "portfolio-1v09pj617-leweprojects-projects.vercel.app";
+    NEXT_PUBLIC_DOMAIN: "www.marvellportfolio.site";
+   // NEXT_PUBLIC_DOMAIN: "portfolio-1v09pj617-leweprojects-projects.vercel.app";
     NODE_ENV: 'development' | 'production';
   }
 }
