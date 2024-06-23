7bed5742171c4196dac5f83554023ddc8882e768
Marvell Ayon <loewiayon12@gmail.com>
2024-06-24 04:14:26 +0800

    env value change

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        URL: "portfolio-1v09pj617-leweprojects-projects.vercel.app",
+        URL: "www.marvellportfolio.site",
       }
 };
 
