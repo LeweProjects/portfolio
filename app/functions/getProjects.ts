c996dff544728138c50eec7a2a4bd481cd9f397c
Marvell Ayon <loewiayon12@gmail.com>
2024-07-05 17:57:27 +0800

    getProjects error handling

--- app/functions/getProjects.ts
+++ app/functions/getProjects.ts
@@ -2,16 +2,16 @@
 import { promises as fs } from "fs";
 
 const getProjects = async () => {
-	const file = await fs.readFile(
-		process.cwd() + "/components/json/projects.json",
-		"utf-8"
-	);
-	const data = JSON.parse(file);
-
-	if (!data.ok) {
-		throw new Error("Failed to fetch data");
-	}
-	return data;
+  const file = await fs.readFile(
+    process.cwd() + "/components/json/projects.json",
+    "utf-8",
+  );
+  const data = JSON.parse(file);
+  
+  if(!data.ok){
+    throw new Error('Failed to fetch data')
+  }
+  return data;
 };
 
 export default getProjects;
