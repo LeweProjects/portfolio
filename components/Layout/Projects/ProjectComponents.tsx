d5ba93d9eb120c82354e2350f4783628c573685c
Marvell Ayon <loewiayon12@gmail.com>
2024-07-08 15:27:20 +0800

    redesigned project components

--- components/Layout/Projects/ProjectComponents.tsx
+++ components/Layout/Projects/ProjectComponents.tsx
@@ -27,51 +27,42 @@ interface projectProps {
 const Portfolio: React.FC<projectProps> = ({ projects }) => {
 	const { projectsArray } = useJsonMapping();
 	return projectsArray.map((data, i) => (
-		<div key={i} className="flex flex-col items-center mb-10">
-			{data.status == "open" ? (
-				i == 1 ? (
+		<div id="projects" key={i} className="flex flex-col items-center mb-10">
+			<div
+				className="w-[50vh] lowercase h-[25vh] peer/project  projImage absolute z-10 opacity-0 flex flex-col space-y-3 items-center 
+							justify-center select-none hover:opacity-100 [&:not(:hover)]:delay-0 delay-200 transition duration-200 scale-110 ease-in bg-black"
+			>
+				<h1 className="font-semibold uppercase">{data.name}</h1>
+				<p className="text-center pb-7">{data?.description}</p>
+				{data.status == "open" ? (
 					<Link
-					
-						id="projects"
 						onClick={() => {
 							data.link == "/" && window.location.reload();
 						}}
-						href={data.link}
-						className="w-[50vh] lowercase h-[25vh] projImage box absolute z-10 opacity-0 flex items-center justify-center hover:cursor-pointer text-6xl hover:opacity-100 transition duration-200 ease-in bg-black"
+						href={data?.link}
+						className="text-xl border-2 hover:bg-white hover:text-black hover:scale-110 transition duration-200 border-white px-2 rounded"
 					>
-						Visit
+						visit site
 					</Link>
 				) : (
-					<Link
-						onClick={() => {
-							data.link == "/" && window.location.reload();
-						}}
-						href={data.link}
-						className="w-[50vh] lowercase h-[25vh] projImage box absolute z-10 opacity-0 flex items-center justify-center hover:cursor-pointer text-6xl hover:opacity-100 transition duration-200 ease-in bg-black"
-					>
-						Visit
-					</Link>
-				)
-			) : (
-				<div
-					//onClick={() => window.location.reload()}
-					className="w-[50vh] lowercase cursor-wait h-[25vh] projImage box absolute z-10 opacity-0 flex items-center justify-center hover:cursor-pointer text-6xl hover:opacity-100 transition duration-200 ease-in bg-black"
-				>
-					Ongoing
-				</div>
-			)}
+					<div className="text-xl border-2 hover:bg-red-400 hover:text-black border-red-400 hover:scale-110 transition duration-200 px-2 rounded text-red-400 cursor-not-allowed">
+						maintenance
+					</div>
+				)}
+			</div>
+
 			<div
-				className={`w-[50vh] h-[25vh] projImage box bg-[length:50vh_25vh] 
-     						bg-no-repeat bg-white`}
+				className="w-[50vh] h-[25vh] projImage bg-[length:50vh_25vh] [&:not(:hover)]:delay-200 delay-0 transition duration-150 peer-hover/project:scale-110
+     						bg-no-repeat bg-white"
 				style={{
 					backgroundImage: `url(${images[data.image]?.src})`,
 				}}
 			></div>
-			<h1 id="projects" className="mt-2 text-xl font-semibold">
+			{/* <h1 id="projects" className="mt-2 text-xl font-semibold">
 				{data?.name}
 			</h1>
 			<p className="w-[50vh] des">{data?.description}</p>
-			<MoreDetails tools={data?.tools} />
+			<MoreDetails tools={data?.tools} /> */}
 		</div>
 	));
 };
